0c003a64a7ce4442facecd9bd8f35b94a5106507
Paulo Cesar Dal Ponte <paulocesardalponte@gmail.com>
2025-09-18 19:51:24 -0300

    factory method

--- desafio01/index02.js
+++ desafio01/index02.js
@@ -1,1 +1,47 @@
-console.log("Hello, World! 002");
+class BicicletaClass {
+  move() {
+    return console.log("Pedalando a bicicleta no pátio da UNISATC");
+  }
+}
+
+class PatineteClass {
+  move() {
+    return console.log("Andando de patinete pelo estacionamento da UNISATC");
+  }
+}
+
+class OnibusClass {
+  move() {
+    return console.log("Andando de ônibus e chegando na UNISATC");
+  }
+}
+
+class TransporteFactory {
+  static tipos = {
+    bicicleta: BicicletaClass,
+    patinete: PatineteClass,
+    onibus: OnibusClass,
+  };
+
+  static criaTipoTransporte(tipo) {
+    const TransporteClass = this.tipos[tipo];
+    if (!TransporteClass) {
+      throw new Error("Tipo ainda não cadastrado!");
+    }
+    return new TransporteClass();
+  }
+}
+
+function main() {
+  const transportes = ["bicicleta", "patinete", "onibus"];
+  try {
+    transportes.forEach((tipo) => {
+      const transporte = TransporteFactory.criaTipoTransporte(tipo);
+      transporte.move();
+    });
+  } catch (err) {
+    console.error("Erro ao localizar transporte...", err.message);
+  }
+}
+
+main();
