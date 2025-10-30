5522e2227d3661d9fba2650b0416ff0e81f3b38b
Paulo Cesar Dal Ponte <paulocesardalponte@gmail.com>
2025-10-30 19:18:44 -0300

    prova02

--- desafio02/index01.js
+++ desafio02/index01.js
@@ -1,1 +1,45 @@
-console.log("Hello, World! 001");
+class LegacyPaymentSystem {
+  makePayment(amount) {
+    console.log(`Pagando R$${amount} com sistema legado.`);
+  }
+}
+
+class ModernPaymentAPI {
+  process(amountInCents) {
+    console.log(`Pagamento de R$${amountInCents / 100} via API moderna.`);
+  }
+}
+
+class PaymentProcessor {
+  constructor(paymentSystem) {
+    this.paymentSystem = paymentSystem;
+  }
+
+  pay(amount) {
+    this.paymentSystem.makePayment(amount);
+  }
+}
+
+class ModernPaymentAdapter {
+  constructor(modernApi) {
+    this.modernApi = modernApi;
+  }
+
+  makePayment(amount) {
+    const amountInCents = amount * 100;
+    this.modernApi.process(amountInCents);
+  }
+}
+
+console.log("Pagamento Legado");
+const legacy = new LegacyPaymentSystem();
+const legacyProcessor = new PaymentProcessor(legacy);
+legacyProcessor.pay(100); 
+
+console.log("\nPagamento Moderno");
+
+const modernApi = new ModernPaymentAPI();
+const adapter = new ModernPaymentAdapter(modernApi); 
+const modernProcessor = new PaymentProcessor(adapter); 
+
+modernProcessor.pay(100);
--- desafio02/index02.js
+++ desafio02/index02.js
@@ -1,1 +1,41 @@
-console.log("Hello, World! 002");
+class TV {
+  turnOn() {
+    console.log("TV ligada.");
+  }
+  turnOff() {
+    console.log("TV desligada.");
+  }
+}
+
+class Radio {
+  turnOn() {
+    console.log("Rádio ligado.");
+  }
+  turnOff() {
+    console.log("Rádio desligado.");
+  }
+}
+
+class RemoteControl {
+  constructor(device) {
+    this.device = device;
+  }
+
+  pressPowerButton(on) {
+    if (on) this.device.turnOn();
+    else this.device.turnOff();
+  }
+}
+
+const tv = new TV();
+const radio = new Radio();
+
+console.log("Usando controle na TV");
+const remoteForTV = new RemoteControl(tv);
+remoteForTV.pressPowerButton(true);
+remoteForTV.pressPowerButton(false);
+
+console.log("\nUsando o MESMO controle no Rádio");
+const remoteForRadio = new RemoteControl(radio);
+remoteForRadio.pressPowerButton(true);
+remoteForRadio.pressPowerButton(false);
