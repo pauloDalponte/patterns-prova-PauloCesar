1193feb214cc83e2fd1063efb3b18dbf9accd565
Paulo Cesar Dal Ponte <129686037+pauloDalponte@users.noreply.github.com>
2025-11-26 19:43:57 -0300

    Update index03.js

--- desafio03/index03.js
+++ desafio03/index03.js
@@ -1,1 +1,21 @@
-console.log("Hello, World! 003");
+class Produto {
+  constructor(preco) {
+    this.preco = preco;
+  }
+
+  calcularPreco() {
+    throw new Error("Implementar");
+  }
+}
+
+class ProdutoComida extends Produto {
+  calcularPreco() {
+    return this.preco * 0.9;
+  }
+}
+
+class ProdutoEletronico extends Produto {
+  calcularPreco() {
+    return this.preco + 20;
+  }
+}
